3cbe4417765b826d8271bc36a7d2960d97333e5f
Viacheslav009 <mstislavpushkov@gmail.com>
2023-05-25 18:57:37 +0300

    commit

---
+++ src/js/foundation.js
@@ -0,0 +1,128 @@
+// !!! картинки и массив обьектов для секции поддержка
+
+
+import save1x from '../images/foundations/save-the-children@1x.png';
+import save2x from '../images/foundations/save-the-children@2x.png';
+import hope1x from '../images/foundations/project-hope@1x.png';
+import hope2x from '../images/foundations/project-hope@2x.png';
+import united1x from '../images/foundations/united@1x.png';
+import united2x from '../images/foundations/united@2x.png';
+import medicalCorp1x from '../images/foundations/medical-corp@1x.png';
+import medicalCorp2x from '../images/foundations/medical-corp@2x.png';
+import msf1x from '../images/foundations/msf@1x.png';
+import msf2x from '../images/foundations/msf@2x.png';
+import razom1x from '../images/foundations/razom@1x.png';
+import razom2x from '../images/foundations/razom@2x.png';
+import aah1x from '../images/foundations/against-hunger@1x.png';
+import aah2x from '../images/foundations/against-hunger@2x.png';
+import worldVision1x from '../images/foundations/world-vision@1x.png';
+import worldVision2x from '../images/foundations/world-vision@2x.png';
+import sprytula1x from '../images/foundations/sprytula@1x.png';
+import sprytula2x from '../images/foundations/sprytula@2x.png';
+
+
+ const charityItems = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: save1x,
+    imgRetina: save2x,
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: hope1x,
+    imgRetina: hope2x,
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: united1x,
+    imgRetina: united2x,
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: medicalCorp1x,
+    imgRetina: medicalCorp2x,
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: msf1x,
+    imgRetina: msf2x,
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: razom1x,
+    imgRetina: razom2x,
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: aah1x,
+    imgRetina: aah2x,
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: worldVision1x,
+    imgRetina: worldVision2x,
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: sprytula1x,
+    imgRetina: sprytula2x,
+  },
+]
+
+
+//  отрисовка списка поддержки
+
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+
+
+const charityEl = document.querySelector('.swiper-wrapper');
+const charityItemsHtml = charityItems.map(({ url, img, imgRetina, title }, index) =>
+  `<div class="charity-item swiper-slide tooltip">
+    <a href="${url}" class="charity-link" 
+      target="_blank"
+      rel="noopener noreferrer nofollow">
+    <span class="charity_index">${(index + 1).toString().padStart(2, "0")}</span>
+     <img class="charity_logo"  srcset = "
+        ${img} 1x,
+        ${imgRetina} 2x
+        "
+        src="${img}" alt="${title}">
+    </a>
+    <span class="tooltiptext tooltip-right">
+    ${title}
+    </span>
+  </div>`).join('');
+
+charityEl.innerHTML = charityItemsHtml;
+const swiperEl = document.querySelector('.swiper');
+
+const swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 4,
+  breakpoints: {
+    768: {
+      slidesPerView: 6
+    }
+  }
+});
+
+const nextBtnEl = document.querySelector('.charity-next-slide');
+nextBtnEl.addEventListener('click', () => {
+  if (swiper.activeIndex + swiper.loopedSlides + 1 > swiper.slides.length) {
+    swiper.slideToLoop(0)
+  } else {
+    swiper.slideNext()
+  }
+});
