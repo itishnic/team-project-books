e9f40541d23b3c9bf73b0ac067b46471d6b4e33b
Viacheslav009 <mstislavpushkov@gmail.com>
2023-08-07 08:08:57 +0300

    commit

--- src/js/get-category-list.js
+++ src/js/get-category-list.js
@@ -7,29 +7,29 @@ export function getCategoryList() {
 }
 // ------ отрисовываем категории книг------//
 
-const categoriesList = document.querySelector('.categories__list');
+// const categoriesList = document.querySelector('.categories__list');
 
-async function showCategories() {
-  try {
-    const response = await getCategoryList();
-    const categories = response.data;
-    const markup = categories
-      .map(category => createCategoryItemMarkup(category.list_name))
-      .join(' ');
+// async function showCategories() {
+//   try {
+//     const response = await getCategoryList();
+//     const categories = response.data;
+//     const markup = categories
+//       .map(category => createCategoryItemMarkup(category.list_name))
+//       .join(' ');
 
-    categoriesList.insertAdjacentHTML('beforeend', markup);
-  } catch (error) {
-    console.log(error);
-  }
-}
-showCategories();
+//     categoriesList.insertAdjacentHTML('beforeend', markup);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// showCategories();
 
-function createCategoryItemMarkup(category) {
-  return `
-    <li class="categories__item">
-      <a class="categories__link js-click-link" data-value="${category}">
-        ${category}
-       </a>
-    </li>
-  `;
-}
+// function createCategoryItemMarkup(category) {
+//   return `
+//     <li class="categories__item">
+//       <a class="categories__link js-click-link" data-value="${category}">
+//         ${category}
+//        </a>
+//     </li>
+//   `;
+// }
