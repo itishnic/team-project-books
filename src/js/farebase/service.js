31c115c2566e33aedf1b0a0a6c818a7c392a5b86
Viacheslav009 <mstislavpushkov@gmail.com>
2023-08-23 07:56:42 +0300

    commit

--- src/js/farebase/service.js
+++ src/js/farebase/service.js
@@ -2,3 +2,25 @@
 // import { firebaseConfig } from './config';
 
 // const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCK9mH8GgpmEiaZIblokYTZ2aP61OZbdmk',
+  authDomain: 'team-project-books.firebaseapp.com',
+  projectId: 'team-project-books',
+  storageBucket: 'team-project-books.appspot.com',
+  messagingSenderId: '1583734977',
+  appId: '1:1583734977:web:127e47f2217667205bf440',
+  measurementId: 'G-KW9HCRM5RZ',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
