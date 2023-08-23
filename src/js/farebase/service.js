a7f660f4639fac5f0dd92da0eeb001a9c87254e6
Viacheslav009 <mstislavpushkov@gmail.com>
2023-08-23 07:51:22 +0300

    commit

--- src/js/farebase/service.js
+++ src/js/farebase/service.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from './config';
+// import { initializeApp } from 'firebase/app';
+// import { firebaseConfig } from './config';
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
