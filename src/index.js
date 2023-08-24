5071c7cb5bfed930e7e4860f3caa881f23c4908c
Viacheslav009 <mstislavpushkov@gmail.com>
2023-08-24 22:03:47 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -7,3 +7,18 @@ import './js/modals';
 import './js/scroll-up-btn';
 import './js/farebase/service';
 import './js/firebase-modal';
+
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCK9mH8GgpmEiaZIblokYTZ2aP61OZbdmk',
+  authDomain: 'team-project-books.firebaseapp.com',
+  projectId: 'team-project-books',
+  storageBucket: 'team-project-books.appspot.com',
+  messagingSenderId: '1583734977',
+  appId: '1:1583734977:web:127e47f2217667205bf440',
+  measurementId: 'G-KW9HCRM5RZ',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
