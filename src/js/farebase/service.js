16ea178056a6ac7268a1e52944948c2913a5bc5a
Viacheslav009 <mstislavpushkov@gmail.com>
2023-08-26 19:27:19 +0300

    commit

--- src/js/farebase/service.js
+++ src/js/farebase/service.js
@@ -1,35 +1,21 @@
-// import { initializeApp } from 'firebase/app';
-// import { firebaseConfig } from './config';
-
-// const app = initializeApp(firebaseConfig);
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-// import { getAnalytics } from 'firebase/analytics';
+import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseApp = initializeApp({
+const firebaseConfig = {
   apiKey: 'AIzaSyCK9mH8GgpmEiaZIblokYTZ2aP61OZbdmk',
   authDomain: 'team-project-books.firebaseapp.com',
   projectId: 'team-project-books',
   storageBucket: 'team-project-books.appspot.com',
   messagingSenderId: '1583734977',
-  appId: '1:1583734977:web:127e47f2217667205bf440',
-  measurementId: 'G-KW9HCRM5RZ',
-});
-const auth = getAuth(firebaseApp);
+  appId: '1:1583734977:web:ece6e7e1e97dd4085bf440',
+  measurementId: 'G-Q7KC86TSVP',
+};
 
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-onAuthStateChanged(auth, user => {
-  if (user !== null) {
-    console.log('logged in!');
-  } else {
-    console.log('No user');
-  }
-});
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
